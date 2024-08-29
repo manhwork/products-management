110b08062f6c11f00eb0ce1346c27502fe707f0b
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-08-29 15:57:49 +0700

    TinyMCE thêm chức năng cho phần description

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const path = require('path');
 
 require('dotenv').config();
 const database = require('./config/database');
@@ -40,6 +41,11 @@ app.set('views', `${__dirname}/views`);
 app.set("view engine", "pug");
 
 app.use(express.static(`${__dirname}/public`));
+
+// Tiny MCE
+// https://www.tiny.cloud/docs/tinymce/latest/expressjs-pm/
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End Tiny MCE
 
 route(app);
 routeAdmin(app);
