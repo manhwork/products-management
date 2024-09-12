896ada8926b53247ae61b58c824b05689c4d8da0
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-12 18:18:27 +0700

    thêm trang tạo mới nhóm quyền và hiện thị nhóm quyền

---
+++ controllers/admin/roles.controller.js
@@ -0,0 +1,31 @@
+const Role = require("../../models/roles.model");
+const systemConfig = require("../../config/system");
+// [GET] /admin/roles
+module.exports.index = async (req, res) => {
+    const find = {
+        deleted: false,
+    };
+
+    const records = await Role.find(find);
+
+    res.render("admin/pages/roles/index.pug", {
+        pageTitle: "Nhóm quyền",
+        records: records,
+    });
+};
+
+// [GET] /admin/roles/create
+
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create.pug", {
+        pageTitle: "Tạo nhóm quyền",
+    });
+};
+// [POST] /admin/roles/create
+
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,12 +1,14 @@
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productsCategoryRoutes = require("./products-category.route");
+const rolesRoutes = require("./roles.route");
 const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
-  const path_admin = systemConfig.prefixAdmin;
+    const path_admin = systemConfig.prefixAdmin;
 
-  app.use(path_admin + "/dashboard", dashboardRoutes);
-  app.use(path_admin + "/products", productRoutes);
-  app.use(path_admin + "/products-category", productsCategoryRoutes);
+    app.use(path_admin + "/dashboard", dashboardRoutes);
+    app.use(path_admin + "/products", productRoutes);
+    app.use(path_admin + "/products-category", productsCategoryRoutes);
+    app.use(path_admin + "/roles", rolesRoutes);
 };
