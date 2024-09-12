18005dfdc13fad82e9497f42685084fdd4618513
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-12 17:21:10 +0700

    tạo collection roles

---
+++ models/roles.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+
+mongoose.plugin(slug);
+
+const roleSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        permissions: {
+            type: Array,
+            default: [],
+        },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+    },
+    {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
+    }
+);
+
+const Role = mongoose.model("Role", roleSchema, "roles");
+
+module.exports = Role;
