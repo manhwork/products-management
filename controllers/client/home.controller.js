0334877c07d2ae28e9f5cf1196eb412517ca3a8c
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-29 16:19:34 +0700

    Tạo phần sub menu

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,15 @@
+const ProductCategory = require("../../models/products-category.model");
+const phanCapHelper = require("../../helpers/phanCap");
 // [GET] /
-module.exports.index = (req, res) => {
-    res.render('client/pages/home/index',{
-        pageTitle: "Trang chu"
+module.exports.index = async (req, res) => {
+    find = {
+        deleted: false,
+    };
+    const records = await ProductCategory.find(find);
+    const newRecords = phanCapHelper(records);
+    // console.log(newRecords);
+    res.render("client/pages/home/index", {
+        pageTitle: "Trang chu",
+        records: newRecords,
     });
-} 
-
+};
