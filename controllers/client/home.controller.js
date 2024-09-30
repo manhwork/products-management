01fe61d16dc38e8ae1f4f4f3b317f6018d49d2b5
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-30 14:36:52 +0700

    hiện thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -4,6 +4,7 @@ const Product = require("../../models/product.model");
 const productsHelper = require("../../helpers/products");
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Hiện thị sản phẩm nổi bật
     find = {
         deleted: false,
         featured: "1",
@@ -11,8 +12,21 @@ module.exports.index = async (req, res) => {
     };
     let productsFeatured = await Product.find(find).limit(6);
     productsFeatured = productsHelper.newPrice(productsFeatured);
+    // End Hiện thị sản phẩm nổi bật
+
+    // Hiển thị sản phẩm mới nhất
+    const newProducts = await Product.find({
+        deleted: false,
+        status: "active",
+    })
+        .sort({ position: "desc" })
+        .limit(6);
+
+    const newProductsNew = productsHelper.newPrice(newProducts);
+    // End Hiển thị sản phẩm mới nhất
     res.render("client/pages/home/index", {
         pageTitle: "Trang chu",
         productsFeatured: productsFeatured,
+        newProducts: newProductsNew,
     });
 };
